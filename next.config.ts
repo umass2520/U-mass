d7648e2974794d1ce4331175b68051e6df032e77
treenuch samanvijit <umass2025@gmail.com>
2026-05-04 19:48:05 +0700

    changed config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enables static export
-  output: 'export', 
-  
-  // Disables the default image optimization API since there is no Node.js server
+  output: 'export',
   images: {
     unoptimized: true,
   },
-
-  // IMPORTANT: If your repository is named something like "my-next-app" 
-  // and your URL will be "https://<username>.github.io/my-next-app/",
-  // you MUST uncomment the line below and replace 'my-next-app' with your repo name.
-  // basePath: '/my-next-app',
+  // This tells Next.js to look for files inside your repo sub-folder
+  basePath: '/U-mass', 
 };
 
 export default nextConfig;
