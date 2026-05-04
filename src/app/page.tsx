a426acc413cac4d3736ca50caef4d8296e6404ee
treenuch samanvijit <umass2025@gmail.com>
2026-05-04 20:11:19 +0700

    added path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className="fixed inset-0 h-full w-full items-center justify-center">
       <iframe
-        src="/umass_pdf.pdf#toolbar=0&navpanes=0&scrollbar=0"
+        src="/Umass/umass_pdf.pdf#toolbar=0&navpanes=0&scrollbar=0"
         className="h-full w-full border-none"
         title=""
       />
