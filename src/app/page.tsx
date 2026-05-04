31af6f07e7ce964445a5f92c4ebc02f2fdb260e4
treenuch samanvijit <umass2025@gmail.com>
2026-05-04 20:06:33 +0700

    fixed2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="fixed inset-0 h-screen w-screen bg-slate-100">
+    <div className="fixed inset-0 h-full w-full items-center justify-center">
       <iframe
         src="/umass_pdf.pdf#toolbar=0&navpanes=0&scrollbar=0"
         className="h-full w-full border-none"
